6425d82d4be021967825be44952f8760f9b18a7d
Epsiodes <episodes27@gmail.com>
2017-11-13 02:45:31 +1100

    退回laravel 5.4
    退回laravel 5.4
    为了支持php5.6.*版本

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,19 +1,18 @@
 <?php
 
-use Faker\Generator as Faker;
-
 /*
 |--------------------------------------------------------------------------
 | Model Factories
 |--------------------------------------------------------------------------
 |
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
