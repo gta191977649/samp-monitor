5b54890952961833bbfd063d8634583e9c334645
ヌルポ <episodes27@gmail.com>
2018-07-18 15:14:47 +0800

    title update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>GTA SAMP服务器导航列表 - Powered by Project Sparrow</title>
+	<title>GTA SA-MP服务器导航列表 - Powered by Project Sparrow</title>
 
     
     {{--
