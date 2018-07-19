7a72e4396541f3656b9d769bad94144c354f2f67
ヌルポ <episodes27@gmail.com>
2018-07-19 14:46:31 +0800

    优化了服务器取得算法

--- app/Console/Commands/SampStatusUpdate.php
+++ app/Console/Commands/SampStatusUpdate.php
@@ -44,52 +44,60 @@ class SampStatusUpdate extends Command
         {
             if($server->failTimes < 48)
             {
-                
-                $this->output->write('Process server  = '.$server->hostname."\n", false);
 
-                $query = new SampQuery($server->ip,$server->port);
+                try{
+                    $this->output->write('Process server  = '.$server->hostname."\n", false);
 
-                if($query->connect()) //如果在线
-                {
-                    $info = $query->getInfo();
-                    $rule = $query->getRules();
-                    $ping = $query->getPing();
-                    
-                    
-                    $server->update([
-                        "hostname" => $server->hostname == $info["hostname"] || empty($info["hostname"]) ? $server->hostname : $info["hostname"],
-                        "gamemode" => $server->gamemode == $info["gamemode"] || empty($info["gamemode"]) ? $server->gamemode : $info["gamemode"],
-                        "map" => $server->map == $rule["mapname"] || empty($rule["mapname"]) ? $server->map : $rule["mapname"],
-                        "version" => $server->version == $rule["version"] || empty($rule["version"]) ? $server->version : $rule["version"],
-                        "weburl" => $server->weburl == $rule["weburl"] || empty($rule["weburl"]) ? $server->weburl : $rule["weburl"],
-                        "maxplayers" => $server->maxplayers == $info["maxplayers"] || $info["maxplayers"] == '' ? $server->maxplayers : $info["maxplayers"],
-                        "failTimes" => 0,
-                    ]);
+                    $query = new SampQuery($server->ip,$server->port);
 
-                    $server->status()->create([
-                        "player" => $info["players"] ? $info["players"] : 0 ,
-                        "ping" => $ping,
+                    if($query->connect()) //如果在线
+                    {
+                        $info = $query->getInfo();
+                        $rule = $query->getRules();
+                        $ping = $query->getPing();
                         
-                        "timeout" => false,
-                    ]); 
-                    
+                        
+                        $server->update([
+                            "hostname" => $server->hostname == $info["hostname"] || empty($info["hostname"]) ? $server->hostname : $info["hostname"],
+                            "gamemode" => $server->gamemode == $info["gamemode"] || empty($info["gamemode"]) ? $server->gamemode : $info["gamemode"],
+                            "map" => $server->map == $rule["mapname"] || empty($rule["mapname"]) ? $server->map : $rule["mapname"],
+                            "version" => $server->version == $rule["version"] || empty($rule["version"]) ? $server->version : $rule["version"],
+                            "weburl" => $server->weburl == $rule["weburl"] || empty($rule["weburl"]) ? $server->weburl : $rule["weburl"],
+                            "maxplayers" => $server->maxplayers == $info["maxplayers"] || $info["maxplayers"] == '' ? $server->maxplayers : $info["maxplayers"],
+                            "failTimes" => 0,
+                        ]);
+
+                        $server->status()->create([
+                            "player" => $info["players"] ? $info["players"] : 0 ,
+                            "ping" => $ping,
+                            "timeout" => false,
+                        ]); 
+                        
+                        continue;
+                    }
+                    else {
+                        $this->onQueryFails($server);
+                        continue;
+                        
+                    }    
+                } catch (\Exception $e) {
+                    $this->onQueryFails($server);
                     continue;
                 }
-                else
-                {
-                    $server->status()->create([
-                        "player" => 0,
-                        "ping" => 0,
-                        "timeout" => true,
-                    ]);
-                    $server->update([
-                        "failTimes" => $server->failTimes+1
-                    ]);
-                    continue;
-                    
-                }    
             }
         }  
  
+    }
+
+    function onQueryFails($server) {
+        $this->output->write('Process server  = '.$server->hostname." 取得失败.\n", false);
+        $server->status()->create([
+            "player" => 0,
+            "ping" => 0,
+            "timeout" => true,
+        ]);
+        $server->update([
+            "failTimes" => $server->failTimes+1
+        ]);
     }
 }
